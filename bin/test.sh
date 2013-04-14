501ed1312e7aa624214bee54cc39465b1bea52de
John Shahid <jvshahid@gmail.com>
2013-04-14 15:10:18 -0400

    make sure we copy gomock to the temp directory.

--- bin/test.sh
+++ bin/test.sh
@@ -10,5 +10,13 @@ bindir=$(dirname $(readlink -f $0))
 
 go get -v "launchpad.net/gocheck"
 
+rm -rf /tmp/gomock
+
 export GOMOCK_TEST_ENV=gomock   # make sure we pass the environment properly
-$bindir/run.sh test && $bindir/run.sh testc
+
+function test_package {
+    rm -rf /tmp/gomock
+    $bindir/run.sh $1
+}
+
+test_package test && test_package testc && test_package testnomock
---
+++ src/testnomock/somefunctions.go
@@ -0,0 +1,6 @@
+package testnomock
+
+// make sure gomock is copied to the temp directory when a package doesn't import
+// gomock.
+
+func SomeFunction() {}
