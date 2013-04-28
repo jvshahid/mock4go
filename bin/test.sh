156e2a6c9beed04f908888fc87ccbcf071dd5a16
John Shahid <jvshahid@gmail.com>
2013-04-28 15:49:43 -0400

    fix the scripts.

--- bin/test.sh
+++ bin/test.sh
@@ -4,19 +4,21 @@ dst=src/github.com/jvshahid
 mkdir -p $dst
 ln -f -s $PWD $dst/
 
-bindir=$(dirname $(readlink -f $0))
+cd `dirname $0`/..
 
-. $bindir/exports.sh
+. bin/exports.sh
 
 go get -v "launchpad.net/gocheck"
 
 rm -rf /tmp/gomock
 
+echo "gopath: $GOPATH"
+
 export GOMOCK_TEST_ENV=gomock   # make sure we pass the environment properly
 
 function test_package {
     rm -rf /tmp/gomock
-    $bindir/run.sh $1
+    go run gomock/gomock.go "$@"
 }
 
 if ! (test_package test && test_package testc && test_package testnomock); then
